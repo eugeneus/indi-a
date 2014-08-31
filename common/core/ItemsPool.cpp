ad3798b921de013e9fa65de557585961de7b38fc
Aliaksander Sinkevich <sinkevicha@gmail.com>
2014-08-31 17:43:10 +0300

    add ItemsPool to manage items

---
+++ common/core/ItemsPool.cpp
@@ -0,0 +1,18 @@
+
+#include "ItemsPool.h"
+
+ItemsPool::ItemsPool(){}
+
+
+ItemsPool::~ItemsPool() {}
+
+ItemsPool* ItemsPool::create() {
+
+   return nullptr;
+}
+
+bool ItemsPool::init()
+{
+   return false;
+}
+
---
+++ common/core/ItemsPool.h
@@ -0,0 +1,23 @@
+
+#ifndef __BlindChef__ItemsPool__
+#define __BlindChef__ItemsPool__
+
+class ItemsPool {
+
+private:
+
+protected:
+
+public:
+
+   ItemsPool();
+
+   ~ItemsPool();
+
+   static ItemsPool* create();
+
+   bool init();
+
+
+};
+#endif /* defined(__BlindChef__ItemsPool__) */
